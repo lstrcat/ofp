55ae7949d5fac6cf1f93b968a5498fe00216c8a2
pq <pq@pq.com>
2021-05-11 16:10:10 +0800

    解压成功

--- mtk_dec.cpp
+++ mtk_dec.cpp
@@ -200,9 +200,50 @@ void ofp::decfile(String fn)
 				
 				String name(pHdr2->name);
 				String filename(pHdr2->filename);
-				LOG(name + " " + filename);
+				
+				LOGHEXDUMP(data,0x60);
 				
 				grid.Add(name,filename,(int64)pHdr2->start,(int64)pHdr2->length,(int64)pHdr2->enclength);
+			
+				FileOut out;
+				if(!out.IsOpen())
+				{
+					String path = "D:/test/" + filename;
+					RealizePath(path);
+					out.Open(path);
+				}
+				if(pHdr2->enclength)
+				{
+					in.Seek(pHdr2->start);
+					StringBuffer b;
+					b.SetLength(pHdr2->enclength);
+					in.Get(b,pHdr2->enclength);
+					if(pHdr2->enclength % 16 != 0)
+					{
+						int n = 16-(pHdr2->enclength % 16);
+						while(n--)
+							b.Cat(0);
+					}
+					std::string encData(~b,b.GetLength());
+					std::string sOut = aes_encrypt_cfb(encData,aeskey,aesiv);
+					out.Put(sOut.c_str(),sOut.length());
+					int64 len = pHdr2->length;
+					len -= pHdr2->enclength;
+					
+					while(len)
+					{
+						int size = 0x200000;
+						if(len < size)
+							size = len;
+						
+						StringBuffer data;
+						data.SetLength(size);
+						in.Get(data,size);
+						len -= size;
+						out.Put(~data,data.GetLength());
+					}
+				}
+				out.Close();
 			}
 		}
 	}
